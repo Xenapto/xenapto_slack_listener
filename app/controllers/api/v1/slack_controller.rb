71cf5d16acdcccbdae33a32cf5b5fb66cec2eb0e
Dominic Sayers <dominic@sayers.cc>
2016-01-26 17:59:49 +0000

    Formatted response

--- app/controllers/api/v1/slack_controller.rb
+++ app/controllers/api/v1/slack_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SlackController < ApplicationController
   def create
-    render plain: params.inspect
+    output = {
+      response_type: :in_channel,
+      text: "You asked for #{params['text']}"
+    }
+
+    render json: output
   end
 end
